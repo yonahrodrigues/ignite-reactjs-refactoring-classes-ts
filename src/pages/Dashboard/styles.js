b3623f790cd898d6c81fe60045a1b546657d4e76
Jonas Rodrigues <baluartesystem@gmail.com>
2022-09-16 18:02:02 -0300

    initial

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const FoodsContainer = styled.div`
   width: 100%;
@@ -6,9 +6,7 @@ export const FoodsContainer = styled.div`
   margin: 0 auto;
   padding: 40px 0;
   margin-top: -140px;
-
   display: grid;
-
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 32px;
 `;
